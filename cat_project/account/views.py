ff36154f9feeefca18d8f7b9ba723d5a88f8d3fc
corykim2 <sos2591@naver.com>
2022-07-27 22:15:30 +0900

    0727 프로젝트 구조 변경

--- cat_project/account/views.py
+++ cat_project/account/views.py
@@ -4,4 +4,4 @@ from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
 
 def main(request):
-    return render(request,'C:/vscode/STRAY CAT/cat_project/templates/main.html')
+    return render(request,'main.html')
