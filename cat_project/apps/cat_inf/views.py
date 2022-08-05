4aea657c57cab2fe1a6aed9faf62b8e0a7b86195
corykim2 <sos2591@naver.com>
2022-08-05 23:28:24 +0900

    css 적용되도록 수정(main.html 참고)

--- cat_project/apps/cat_inf/views.py
+++ cat_project/apps/cat_inf/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, redirect
 from .models import Cat
 from django.utils import timezone
 
-def map2(request):
+def map2(request): #이거 이름 좀 잘 바꾸기 create 같은 거로
     if(request.method == 'POST'):
         post = Cat()
         post.name = request.POST['name']
@@ -16,19 +16,3 @@ def map2(request):
         post.save()
         return redirect('main')
     return render(request,'cat_inf/map2.html')
-
-
-def create(request):
-    if(request.method == 'POST'):
-        post = Cat()
-        post.name = request.POST['name']
-        post.date = timezone.now()
-        post.info2 = request.POST['info2']
-        post.info3 = request.POST['info3']
-        post.info4 = request.POST['info4']
-        post.latitude = request.POST['latitude']
-        post.longitude = request.POST['longitude']
-        post.save()
-    else:
-        pass
-    return redirect('main')
